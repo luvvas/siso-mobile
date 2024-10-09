c7061f56dc51ce453768ef8c5d89729d8accc40d
Lucas <lucas.machado@isocrm.com.br>
2024-10-09 17:31:36 -0300

    feat: example service api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.example.com', // Substitua pela sua API
+  // TODO: API base
+  baseURL: 'https://api.example.com',
 });
 
 export default api;
