25183eeb903db0f39a31d4d71fb34828971a39a0
Lucas <lucas.machado@isocrm.com.br>
2024-10-16 21:46:05 -0300

    feat: register screen

--- App.tsx
+++ App.tsx
@@ -2,36 +2,39 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button, Text, View } from 'react-native';
+// import { Button, Text, View } from 'react-native';
 
 import LoginForm from './src/screens/LoginForm';
+import RegisterForm from './src/screens/RegisterForm';
 
 const Stack = createNativeStackNavigator();
 
-function HomeScreen({ navigation }: any) {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Welcome to the Home Screen!</Text>
-      <Button title="Go to Login" onPress={() => navigation.navigate('Login')} />
-      <StatusBar style="light" />
-    </View>
-  );
-}
+// function HomeScreen({ navigation }: any) {
+//   return (
+//     <View className="flex-1 items-center justify-center bg-white">
+//       <Text>Welcome to the Home Screen!</Text>
+//       <Button title="Go to Login" onPress={() => navigation.navigate('Login')} />
+//       <StatusBar style="light" />
+//     </View>
+//   );
+// }
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="inverted" />
       <Stack.Navigator>
-        <Stack.Screen 
+        {/* <Stack.Screen 
           name="Home" 
           component={HomeScreen} 
           options={{ headerShown: false }} 
-        />
+        /> */}
         <Stack.Screen 
           name="Login" 
           component={LoginForm} 
           options={{ headerShown: false }} 
         />
+        <Stack.Screen name="Register" component={RegisterForm} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
